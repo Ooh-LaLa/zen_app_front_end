cf7e809ffa573335a16d63c868c123dde60c4775
ooh-lala <laurentozzi.code@gmail.com>
2023-02-23 18:28:37 -0500

    add profile icon and assets folder

--- src/pages/Profiles/Profiles.tsx
+++ src/pages/Profiles/Profiles.tsx
@@ -7,6 +7,17 @@
 import { Profile } from '../../types/models'
 
 
+// components
+import ProfileCard from '../../components/ProfileCard/ProfileCard'
+
+
+
+
+
+
+
+
+
 interface ProfilesProps {
 	profiles: Profile[]
 }
@@ -15,16 +26,15 @@ interface ProfilesProps {
 
 const Profiles =(props: ProfilesProps): JSX.Element => {
   const { profiles } = props
-  
+
   if(!profiles.length) return <p>No profiles yet</p>
 
   return (
-    <>
-      <h1>Hello. This is a list of all the profiles.</h1>
+    <main className='list'>
       {profiles.map((profile: Profile) =>
-        <p key={profile.id}>{profile.name}</p>
+        <ProfileCard key={profile.id} profile={profile} />
       )}
-    </>
+    </main>
   )
 }
  
