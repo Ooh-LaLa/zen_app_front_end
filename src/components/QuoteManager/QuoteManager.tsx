229ae88281ff5c9ee530b2e73d8534f95c77edbf
ooh-lala <laurentozzi.code@gmail.com>
2023-02-23 20:51:57 -0500

    fix delete account controller

---
+++ src/components/QuoteManager/QuoteManager.tsx
@@ -0,0 +1,23 @@
+// assets
+
+
+
+// types
+import { Profile } from '../../types/models'
+
+interface QuoteManagerProps {
+	profile: Profile
+}
+
+const QuoteManager = (props: QuoteManagerProps): JSX.Element => {
+	const { profile } = props
+
+
+  return (
+    <section>
+ 
+    </section>
+  )
+}
+
+export default QuoteManager
--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,19 +1,36 @@
 // stylesheets
 import styles from './Landing.module.css'
 
+
+//services
+// services
+import * as authService from '../../services/authService'
+
 // types
 import { User } from '../../types/models'
 
 interface LandingProps {
   user: User | null;
+  handleLogout: () => void
 }
 
-const Landing = (props: LandingProps): JSX.Element => {
-  const { user } = props
-
+const Landing =  ({ user, handleLogout }: LandingProps): JSX.Element => {
+  // const { user } = props
+  const handleDeleteAccount = async(): Promise<void> => {
+    await authService.deleteAccount()
+    handleLogout()
+  }
   return (
     <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
+      <h1>hello, {user ? user.name : 'zen master'}</h1>
+      {/* <h1>Meow Meow <br/> Beans</h1>
+      <img src={logo} alt="A meow meow bean" /> */}
+
+      { user && 
+        <button onClick={handleDeleteAccount}>
+          DELETE ACCOUNT
+        </button>
+      }
     </main>
   )
 }
